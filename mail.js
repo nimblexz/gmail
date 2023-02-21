f469e1754eeb5b031be6808235441fa740d1ba82
nimblexz <109966757+nimblexz@users.noreply.github.com>
2023-02-21 21:47:08 +0400

    Initial commit

---
+++ mail.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const nodemailer = require('nodemailer')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+let smtp_login = process.env.SMTP_LOGIN
+let smtp_pass = process.env.SMTP_PASS
+let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: smtp_login,
+        pass: smtp_pass
+    }
+})
+
+app.get('/', function (req, res) {
+    res.send('Hello World');
+})
+
+app.post('/send', async function (req, res) {
+    let {name, eMail, message} = req.body
+    let info = await transporter.sendMail({
+        from: 'HR WANTS ME',
+        to: "pacanchik.555@yandex.ru",
+        subject: 'JOBS',
+        html: `<b>Сообщение с моего портфолио</b>
+<div>name: ${name}</div>
+<div>email: ${eMail}</div>
+<div>message: ${message}</div>
+`
+    })
+    res.send(info)
+
+
+})
+let port = process.env.PORT || 3010
+app.listen(port, function () {
+    console.log('node launch')
+})
